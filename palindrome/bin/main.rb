421c6af32121f2d45ce5a3073802b5f9f5efd62b
paras <paras.saini1992@gmail.com>
2014-08-04 20:21:20 +0530

    Take input from the user and check's if palindrome
    Takes input form the user in a string and check if the input string is either q or Q.
    If the input is q or Q it quits. Else it check's weather the input string is a palindrome or not.
    Then it ask user enter another string or quit.

--- palindrome/bin/main.rb
+++ palindrome/bin/main.rb
@@ -4,8 +4,8 @@ puts "Enter a String "
 while !((string = gets).chomp!.downcase).eql?("q")
   object = Palindrome.new(string)
   if object.palindrome_check?
-    puts "#{ object.string } is a palindrome\nEnter next sting or press(q/Q) to exit"
+    puts "#{ object.string } is a palindrome\nEnter next sting or press (q/Q) to exit"
   else
-    puts "#{ object.string } is not a palindrome\nEnter next string or press(q/Q) to exit"
+    puts "#{ object.string } is not a palindrome\nEnter next string or press (q/Q) to exit"
   end
 end
